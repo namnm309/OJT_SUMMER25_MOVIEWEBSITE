bd80597fa04cf17c7391bbde0e91fc5f21eb5081
namnm309 <137751495+namminhs394@users.noreply.github.com>
2025-06-11 14:29:20 +0700

    add Project

---
+++ MovieWebApp/DomainLayer/Enum/MovieStatus.cs
@@ -0,0 +1,9 @@
+namespace DomainLayer.Enum
+{
+    public enum MovieStatus
+    {
+        SapChieu = 1,    // Sắp chiếu
+        DangChieu = 2,   // Đang chiếu
+        ChuaCo = 3       // Chưa có
+    }
+} 
