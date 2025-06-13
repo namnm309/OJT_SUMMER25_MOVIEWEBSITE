2517f0be5dd8a463766e5ea484714ae4176cde99
Nguyễn Minh Nam <namnmse182871@fpt.edu.vn>
2025-06-13 07:13:57 +0000

    fix conflict

--- MovieWebApp/DomainLayer/Enum/MovieStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace DomainLayer.Enum
-{
-    public enum MovieStatus
-    {
-        SapChieu = 1,    // Sắp chiếu
-        DangChieu = 2,   // Đang chiếu
-        ChuaCo = 3       // Chưa có
-    }
-} 
