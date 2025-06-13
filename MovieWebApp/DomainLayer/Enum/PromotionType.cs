13991a9575de632b0b8903c19a0ddcc88638dbec
Nguyễn Minh Nam <namnmse182871@fpt.edu.vn>
2025-06-13 07:13:25 +0000

    delete 6/13

--- MovieWebApp/DomainLayer/Enum/PromotionType.cs
+++
@@ -1,8 +0,0 @@
-namespace DomainLayer.Enum
-{
-    public enum PromotionType
-    {
-        Percentage = 0,     // Giảm theo phần trăm (%)
-        FixedAmount = 1     // Giảm số tiền cố định (VNĐ)
-    }
-} 
